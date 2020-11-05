25412453b0a96d114451180d17a7f8605e1f2f45
Julia Schafferus <s0559288@htw-berlin.de>
2020-11-05 16:05:58 +0100

    Changed design, added BlogPostTeaser + BlogView

--- packages/blog/src/App.js
+++ packages/blog/src/App.js
@@ -1,22 +1,30 @@
 import './App.css';
-import BoxComponent from "./BoxComponent";
+import BlogPostTeaser from "./BlogPostTeaser";
 
 function App() {
-  return (
-    <div className="App">
-        <div className="Mocked-Header">
-            Header
+    return (
+        <div className="App">
+            <div className="Mocked-Header">
+                Header
+            </div>
+            <div className="Mocked-Sidebar">
+                Sidebar
+            </div>
+            <main className="content">
+                <h1> "Blog Listing" </h1>
+                <div className="BlogPostTeaserList">
+                    <BlogPostTeaser/>
+                    <BlogPostTeaser/>
+                    <BlogPostTeaser/>
+                    <BlogPostTeaser/>
+                    <BlogPostTeaser/>
+                    <BlogPostTeaser/>
+                    <BlogPostTeaser/>
+                    <BlogPostTeaser/>
+                </div>
+            </main>
         </div>
-        <div className="Mocked-Sidebar">
-            Sidebar
-        </div>
-        <main className="content">
-         <h1> "Blog Listing" </h1>
-            <BoxComponent></BoxComponent>
-        </main>
-    {/*<img src={logo} className="App-logo" alt="logo" />*/}
-    </div>
-  );
+    );
 }
 
 export default App;
