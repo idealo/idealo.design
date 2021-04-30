2491fdc4f9f351fec5bb28d41e4f778c28c7255c
Lucia Feddersen <lucia.feddersen@w0epx.fritz.box>
2021-04-30 11:31:07 +0200

    bootstrap deleted

--- packages/idealo.design/src/ui/index.js
+++ packages/idealo.design/src/ui/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import StyleContext from 'isomorphic-style-loader/StyleContext'
-//import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { BrowserRouter as Router } from 'react-router-dom'
 
