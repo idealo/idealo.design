34dc6595ddc23cbc1f3931fa5c826e6895358f0b
MuellerBettina <MuellerBettina@pm.me>
2021-05-10 15:08:31 +0200

    added custom alerts for each use case

--- packages/idealo.design/src/ui/pages/BlogPage/Editor/Editor.jsx
+++ packages/idealo.design/src/ui/pages/BlogPage/Editor/Editor.jsx
@@ -145,13 +145,13 @@ class RichTextEditor extends React.Component {
 
     if (!this.state.title) {
       formIsValid = false;
-      errors['title'] = 1;
+      errors['title-empty'] = 1;
     }
 
     blacklist.map(word => {
       if (word === slugify(this.state.title).replace(/^\s+|\s+$/g, '').toLowerCase()) {
         formIsValid = false;
-        errors ['title'] = 1;
+        errors ['title-value'] = 'Title can not be "new post"';
       }
     });
 
@@ -171,8 +171,28 @@ class RichTextEditor extends React.Component {
 
   handleSubmit(e){
     e.preventDefault();
+    if(!this.handleValidation() && this.state.error['title-value']){
+      alert("Title is invalid. Please choose a different title!");
+      return;
+    }
+
+    if(!this.handleValidation() && this.state.error['title-empty']){
+      alert("Please choose a title!");
+      return;
+    }
+
+    if(!this.handleValidation() && this.state.error['categoryDisplayValue']){
+      alert("Please choose a category!");
+      return;
+    }
+
+    if(!this.handleValidation() && this.state.error['editorState']){
+      alert("Please write something!");
+      return;
+    }
+
     if(!this.handleValidation()){
-      alert("Form has errors.")
+      alert("Form has errors. All fields must be completed.");
       return;
     }
 
@@ -292,7 +312,7 @@ class RichTextEditor extends React.Component {
          : <h1>Create blogpost</h1>}
 
         <div className={s.InputFields}>
-          <input className={this.state.error['title'] ? s.empty : ''} onChange={this.handleChange} name="title" value={this.state.title} placeholder="Titel"/>
+          <input className={this.state.error['title-empty'] ? s.empty : '' || this.state.error['title-value'] ? s.empty : ''} onChange={this.handleChange} name="title" value={this.state.title} placeholder="Titel"/>
           <form name="category" className="select-container">
             <CreatableSelect
                 getOptionLabel={option => option.categorydisplayvalue}
@@ -352,6 +372,12 @@ class RichTextEditor extends React.Component {
         </>
     );
   }
+
+  /*wrongTitle() {
+    span = <span></span>
+    console.log("Der Titel stimmt so nicht")
+    return span;
+  }*/
 }
 
 // Custom overrides for "code" style.
