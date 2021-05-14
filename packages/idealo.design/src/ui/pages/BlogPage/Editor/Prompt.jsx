39f54df0cc79225c310a783386282e71685a5b5e
MuellerBettina <MuellerBettina@pm.me>
2021-05-14 10:22:41 +0200

    changed Button to html button and deleted import we'll be using our own button component in the future

--- packages/idealo.design/src/ui/pages/BlogPage/Editor/Prompt.jsx
+++ packages/idealo.design/src/ui/pages/BlogPage/Editor/Prompt.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactModal from "react-modal";
-import Button from "@storybook/react/dist/demo/Button";
 
 export default function PromptSuccess(props) {
   return (
@@ -24,13 +23,13 @@ export default function PromptSuccess(props) {
           <footer>
               {props.message === 'Do you want to delete or archive that post?'
                   ?<div>
-                      <Button onClick={props.onCancel}>Close</Button>
-                      <Button onClick={props.onArchive}>Archive</Button>
-                      <Button onClick={props.onDelete}>Delete</Button>
+                      <button onClick={props.onCancel}>Close</button>
+                      <button onClick={props.onArchive}>Archive</button>
+                      <button onClick={props.onDelete}>Delete</button>
                   </div>:
                   <div>
-                      <Button onClick={props.onHide}>Close</Button>
-                      <Button onClick={props.onLeave}>Yes</Button>
+                      <button onClick={props.onHide}>Close</button>
+                      <button onClick={props.onLeave}>Yes</button>
                   </div>
               }
           </footer>
