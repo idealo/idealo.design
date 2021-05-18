13dcfcc438020116167000741b015af5f85a6d9e
sennikrauss <sennikrauss@icloud.com>
2021-05-18 23:24:19 +0200

    test new post button

--- packages/idealo.design/src/__tests__/header_login_logout.test.jsx
+++ packages/idealo.design/src/__tests__/header_login_logout.test.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { create } from "react-test-renderer"
 import Search from '../ui/components/Header/Search';
-import ListView from "../ui/pages/BlogPage/ListView";
+import {ListView}from "../ui/pages/BlogPage/ListView";
+import {shallow}  from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {withRouter} from "react-router";
 
 it('login renders correctly', () => {
     const logoutTest = create(<Search/>);
@@ -22,17 +26,46 @@ it('logout renders correctly', done => {
     }
 });
 
-/*// when user is logged in -> user sees new blogpost button
-it('new blogpost button is shown', () => {
-    const listView = create(<ListView />);
-    const instance = listView.getInstance();
-    console.log(instance);
+//when user is logged in -> user sees new blogpost button
+it('new blogpost button is shown', done => {
+    //Enzyme.configure({ adapter: new Adapter() });
+    /*jest.mock('react-router-dom', () => ({
+        useParams: jest.fn().mockReturnValue({ slug: '' }),
+    }));*/
+    // const listView = shallow(<ListView />);
+    try {
+        const listView = create(<ListView />);
+        const instance = listView.getInstance();
+        expect(instance).toMatchSnapshot();
+        done();
+    }catch(error){
+        done(error)
+    }
+
 
 
 });
 
-it('new blogpost button is not shown', () => {
-    // const listView = create(<ListView />);
+/*Enzyme.configure({ adapter: new Adapter() });
+
+describe('new blogpost button is shown', () => {
+
+    jest.mock('react-router', () => ({
+        ...jest.requireActual('react-router'),
+        useParams: jest.fn().mockReturnValue({ slug: '' }),
+    }));
+
+    it('renders', () => {
+        const wrapper = shallow(<ListView />);
+        expect(wrapper).toBeTruthy();
+    });
+
+});*/
+
+/*it('new blogpost button is not shown', () => {
+    const listView = create(<ListView />);
+    const instance = listView.getInstance();
+    console.log(instance)
     expect(true).toBeTruthy();
 
 });*/
--- packages/idealo.design/src/ui/pages/BlogPage/ListView.jsx
+++ packages/idealo.design/src/ui/pages/BlogPage/ListView.jsx
@@ -1,94 +1,97 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import withStyles from 'isomorphic-style-loader/withStyles'
 
-import { fetchList, fetchPostsByCategorySlug, fetchUserInfo } from './data';
+import {fetchList, fetchPostsByCategorySlug, fetchUserInfo} from './data';
 import { Link, useParams, useHistory } from "react-router-dom";
 
 import s from './Blogpage.module.scss';
 import draftToHtml from '../../../vendor/draftjs-to-html';
 import { htmlToText } from 'html-to-text';
+import {withRouter} from "react-router";
 
-function ListView() {
-  const history = useHistory();
-  const [ list, setList ] = useState([]);
-  const [ userInfo, setUserInfo ] = useState([]);
-  const { slug } = useParams();
+export class ListView extends React.Component{
 
-  useEffect(() => {
-    let mounted = true;
-
-    const setData = (list) => {
-      if (mounted) {
-        setList(list);
-      }
+    constructor(props) {
+        super(props);
+        this.state = {
+            userInfo : {},
+            list: []
+        }
+        this.handleNewPost = this.handleNewPost.bind(this)
+        this.componentDidMount = this.componentDidMount.bind(this)
+        this.getReactElement = this.getReactElement.bind(this)
     }
 
-    const setUser = (user) => {
-      if (mounted) {
-        setUserInfo(user);
-      }
+    async componentDidMount() {
+        try{
+            const slug = this.props.match.params.slug
+            this.setState(
+                {
+                    userInfo: await fetchUserInfo()
+                }
+            );
+
+            if(slug){
+                this.setState({
+                    list : await fetchPostsByCategorySlug({categorySlug: slug})
+                })
+            }else {
+                this.setState({
+                    list: await fetchList()
+                })
+            }
+        }catch(error){
+
+        }
     }
 
-    if (slug) {
-      fetchPostsByCategorySlug({ categorySlug: slug })
-        .then(setData);
-    } else {
-      fetchList()
-        .then(setData);
+    handleNewPost(){
+        this.props.history.push("/blog/new-post");
+
     }
-    fetchUserInfo().then(setUser);
 
-    return () => mounted = false;
-  }, [slug]);
+    getReactElement(a){
+        const htmlBlogContent = draftToHtml(a);
+        const plainText = htmlBlogContent.replace(/<[^>]*>/g, '');
+        return htmlToText(plainText);
+    }
 
-  const handleNewPost = () => {
-    history.push({
-      pathname: `/blog/new-post`,
-    });
-  }
+    render(
+    ) {
+        return (
+            <div>
+                {this.state.userInfo.status === 'LOGGED_IN'
+                    ? <button style={{
+                        float: 'right',
+                        marginBottom: '1rem',
+                        marginRight: '1rem',
+                        backgroundColor: '#395F86',
+                        border: 'none',
+                        color: 'white',
+                        padding: '10px 30px',
+                        textAlign: 'center',
+                        fontSize: '14px',
+                    }} onClick={this.handleNewPost}>New Post</button>
+                    : <div> </div>}
 
-  // const highlighted = list.pop()
-  function getReactElement(a){
-    const htmlBlogContent = draftToHtml(a);
-    const plainText = htmlBlogContent.replace(/<[^>]*>/g, '');
-    return htmlToText(plainText);
-  }
+                <div style={{ clear: 'both' }}/>
 
-  return (
-    <>
-      {userInfo.status === 'LOGGED_IN'
-          ? <button style={{
-                float: 'right',
-                marginBottom: '1rem',
-                marginRight: '1rem',
-                backgroundColor: '#395F86',
-                border: 'none',
-                color: 'white',
-                padding: '10px 30px',
-                textAlign: 'center',
-                fontSize: '14px',
-              }} onClick={handleNewPost}>New Post</button>
-          : <div> </div>}
 
-      <div style={{ clear: 'both' }}/>
-    
-
-      <div className={s.List}>
-        {list.map((blogpost) => (
-          <div key={blogpost.id} className={s.Content}>
-            <h2 className={s.blogpostTitle}>
-              <Link to={`/blog/${blogpost.slug}`}>{blogpost.title}</Link>
-            </h2>
-            <div><img alt="" src={blogpost.image}/>
-            {getReactElement(blogpost.blogpostcontent)}
+                <div className={s.List}>
+                    {this.state.list.map((blogpost) => (
+                        <div key={blogpost.id} className={s.Content}>
+                            <h2 className={s.blogpostTitle}>
+                                <Link to={`/blog/${blogpost.slug}`}>{blogpost.title}</Link>
+                            </h2>
+                            <div><img alt="" src={blogpost.image}/>
+                                {this.getReactElement(blogpost.blogpostcontent)}
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
-          </div>
-        ))}
-      </div>
-    </>
-  )
+        )
+    }
 }
-
-
-export default /*withStyles(s)*/(ListView);
+export default withRouter(ListView);
