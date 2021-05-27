e9e421296e96503f01e144fca13daaa2cc6cd99b
iuliiaromanova <romanovajb@gmail.com>
2021-05-27 15:10:58 +0200

    Adding local provider test for one endpoint (fetchList)

--- packages/idealo.design/src/server/index-contract.spec.js
+++ packages/idealo.design/src/server/index-contract.spec.js
@@ -1,12 +1,14 @@
 const { Verifier } = require('@pact-foundation/pact');
+const path = require('path');
 
 describe('Pact Verification', () => {
     test('should validate the expectations of our consumer', () => {
         const opts = {
             provider: 'Provider',
-            providerBaseUrl: 'http://localhost:3000',
-            pactBrokerUrl: '',
-            pactBrokerToken: '',
+            providerBaseUrl: 'http://localhost:8080',
+           // pactBrokerUrl: process.env.PACT_BROKER_URL,
+          //  pactBrokerToken: process.env.PACT_BROKER_TOKEN,
+            pactUrls: [path.resolve(process.cwd(), 'src/__tests__/pact/consumer-provider.json')],
             publishVerificationResult: true,
             providerVersion: '1.0.0',
             logLevel: 'INFO',
