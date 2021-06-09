a69f3fc24af920d389ba7a4b36dd7c1a97cbdb5f
Lucia Feddersen <lucia.feddersen@w0epx.fritz.box>
2021-06-09 18:31:33 +0200

    dropdown filter for technology

--- packages/idealo.design/src/ui/pages/ComponentsPage/ComponentView.jsx
+++ packages/idealo.design/src/ui/pages/ComponentsPage/ComponentView.jsx
@@ -2,109 +2,109 @@ import React from "react";
 import {withRouter} from "react-router";
 import s from './ComponentsPage.module.scss';
 import { ReactComponent as Logo } from '../../../../public/logo.svg';
-import { ReactComponent as Arrow } from '../../../../public/ico_chevron_right.svg'
-import { Dropdown, Selection } from 'react-dropdown-now';
+import { Dropdown } from 'react-dropdown-now';
+
+const components = [
+    {
+        id: 1,
+        name: 'Radio Button',
+        tagAll: '#all',
+        tagImport: '#motive-ui',
+        tagStacks: '#react'
+    },
+    {
+        id: 2,
+        name: 'Select',
+        tagAll: '#all',
+        tagImport: '#motive-ui',
+        tagStacks: '#react'
+    },
+    {
+        id: 3,
+        name: 'Radio Button',
+        tagAll: '#all',
+        tagImport: '#motive-ui',
+        tagStacks: '#classic'
+    },
+    {
+        id: 4,
+        name: 'Radio Button',
+        tagAll: '#all',
+        tagImport: '#figma',
+        tagStacks: '#react'
+    },
+    {
+        id: 5,
+        name: 'Select',
+        tagAll: '#all',
+        tagImport: '#figma',
+        tagStacks: '#react'
+    },
+    {
+        id: 6,
+        name: 'Radio Button',
+        tagAll: '#all',
+        tagImport: '#figma',
+        tagStacks: '#classic'
+    },
+];
 
 class ComponentView extends React.Component {
+
     constructor(props) {
         super(props);
-        this.handleClick = this.handleClick.bind(this);
         this.state = {
-            showMenu: false,
-        }
-        this.showMenu = this.showMenu.bind(this);
-        this.closeMenu = this.closeMenu.bind(this);
-    }
-
-    handleClick() {
-        console.log('Click happened');
-    }
-
-    showMenu(event) {
-        event.preventDefault();
-
-        this.setState({showMenu: true}, () => {
-            document.addEventListener('click', this.closeMenu);
-        });
-    }
-
-    closeMenu() {
-        if (!this.dropdownMenu.contains(event.target)) {
-
-            this.setState({showMenu: false}, () => {
-                document.removeEventListener('click', this.closeMenu);
-            });
-
+            filterValue: 'All Technology',
+            components :[]
         }
     }
 
+    async componentDidMount() {
+        this.setState(
+            this.state.components = components,
+        )
+    }
+
+    setFilter(select) {
+        this.setState( this.state.filterValue = select);
+    }
 
     render() {
         return (
             <div>
-                {/*<div>
-                    <button className={s.dropdown} onClick={this.showMenu}>By Technology </button><arrow/>
-                    {
-                        this.state.showMenu
-                            ? (
-                                <div className="menu"
-                                     ref={(element) => {
-                                         this.dropdownMenu = element;
-                                     }}>
-                                    <div>
-                                        <button> React Stacks</button>
-                                    </div>
-                                    <div>
-                                        <button> Classic Stacks</button>
-                                    </div>
-                                </div>
-                            ) : null
-                    }
-                </div>*/}
                 <div className={s.dropdown}>
                     <Dropdown
-                        placeholder="By Technology"
-                        options={['React Stacks', 'Classic Stacks']}
-                        onChange={(value) => console.log('change!', value)}
+                        placeholder="All Technology"
+                        options={['All Technology','#react', '#classic']}
+                        value='All Technology'
+                        onChange={(value) => this.setFilter(value)}
                         onSelect={(value) => console.log('selected!', value)}
                         onClose={(closedBySelection) => console.log('closedBySelection?:', closedBySelection)}
                     />
                 </div>
-
                 <div className={s.container}>
-                    <div className={s.item}>
-                        <Logo/>
-                        <h2>Radio Button</h2>
-                        #motif-ui #react
-                    </div>
-                    <div className={s.item}>
-                        <Logo/>
-                        <h2>Radio Button</h2>
-                        #motif-ui #react
-                    </div>
-                    <div className={s.item}>
-                        <Logo/>
-                        <h2>Radio Button</h2>
-                        #motif-ui #react
-                    </div>
-                    <div className={s.item}>
-                        <Logo/>
-                        <h2>Radio Button</h2>
-                        #motif-ui #react
-                    </div>
-                    <div className={s.item}>
-                        <Logo/>
-                        <h2>Radio Button</h2>
-                        #motif-ui #react
-                    </div>
-                    <div className={s.item}>
-                        <Logo/>
-                        <h2>Radio Button</h2>
-                        #motif-ui #react
-                    </div>
+                    {   this.state.filterValue.value === 'All Technology' || this.state.filterValue=== 'All Technology' ?
+                            this.state.components.map((components) => (
+                                    <div className={s.item}>
+                                        <Logo className={s.logo}/>
+                                        <h2>{components.name}</h2>
+                                        <div>
+                                            <p>{components.tagImport} {components.tagStacks}</p>
+                                        </div>
+                                    </div>
+                            )) :
+                            this.state.components.filter(components => components.tagStacks === this.state.filterValue.value).map((components) => (
+                                <div className={s.item}>
+                                    <Logo className={s.logo}/>
+                                    <h2>{components.name}</h2>
+                                    <div>
+                                        <p>{components.tagImport} {components.tagStacks}</p>
+                                    </div>
+                                </div>
+                            ))
+                    }
                 </div>
             </div>
-
         );
     }
 }
