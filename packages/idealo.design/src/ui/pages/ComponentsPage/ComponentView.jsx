b701eccc3622fcc88de39c781219e3fd1eae23f0
Lucia Feddersen <lucia.feddersen@w0epx.fritz.box>
2021-06-10 18:52:59 +0200

    multiSelect for tags

--- packages/idealo.design/src/ui/pages/ComponentsPage/ComponentView.jsx
+++ packages/idealo.design/src/ui/pages/ComponentsPage/ComponentView.jsx
@@ -1,44 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import {withRouter} from "react-router";
 import s from './ComponentsPage.module.scss';
-import { ReactComponent as Logo } from '../../../../public/logo.svg';
-import { Dropdown } from 'react-dropdown-now';
-//import { Multiselect } from 'multiselect-react-dropdown';
+import { ReactComponent as Checkbox } from '../../../../public/Checkbox.svg';
+import Select from 'react-select';
 
 const components = [
     {
         id: 1,
-        name: 'Radio Button',
+        name: 'Checkbox1',
         tagImport: '#motive-ui',
-        tagStacks: '#react'
+        tagStacks: '#react',
     },
     {
         id: 2,
         name: 'Select',
         tagImport: '#motive-ui',
-        tagStacks: '#react'
+        tagStacks: '#react',
     },
     {
         id: 3,
-        name: 'Radio Button',
+        name: 'Checkbox2',
         tagImport: '#motive-ui',
         tagStacks: '#classic'
     },
     {
         id: 4,
-        name: 'Radio Button',
+        name: 'Checkbox3',
         tagImport: '#figma',
         tagStacks: '#react'
     },
     {
         id: 5,
-        name: 'Select',
+        name: 'Checkbox4',
         tagImport: '#figma',
         tagStacks: '#react'
     },
     {
         id: 6,
-        name: 'Radio Button',
+        name: 'Checkbox5',
         tagImport: '#figma',
         tagStacks: '#classic'
     },
@@ -46,14 +45,12 @@ const components = [
 
 class ComponentView extends React.Component {
 
-
     constructor(props) {
         super(props);
         this.state = {
-            filterValue: 'All Technology',
-            components : [],
-            //selectedValue : '',
-            //options: ['All Technology','#react', '#classic'],
+            filterValue: ['All'],
+            components: [],
+            options: [],
         }
     }
 
@@ -61,53 +58,57 @@ class ComponentView extends React.Component {
         this.setState(
             this.state.components = components,
         )
+        this.setState(
+            this.state.options = [
+                { label: 'All', value: 'All'},
+                { label: '#react', value: '#react'},
+                { label: '#classic', value: '#classic'},
+                { label: '#figma', value: '#figma'},
+                { label: '#motive-ui', value: '#motive-ui'},
+            ]
+        )
     }
 
     setFilter(select) {
-        this.setState( this.state.filterValue = select);
+        const a = [];
+        select.map((select) => (a.push(select.value)))
+        this.setState( this.state.filterValue = a);
     }
 
     render() {
         return (
             <div>
-                <div className={s.dropdown}>
-                    <Dropdown
-                        placeholder="All Technology"
-                        options={['All Technology','#react', '#classic']}
-                        value='All Technology'
+                <div className={s.multiSelect}>
+                    <Select
+                        isMulti
+                        defaultValue={{ label: 'All', value: 'All'}}
+                        options={this.state.options}
+                        className="basic-multi-select"
                         onChange={(value) => this.setFilter(value)}
-                        onSelect={(value) => console.log('selected!', value)}
-                        onClose={(closedBySelection) => console.log('closedBySelection?:', closedBySelection)}
+                        classNamePrefix="select"
                     />
                 </div>
-                {/*<div>
-                    <Multiselect
-                        options={this.state.options} // Options to display in the dropdown
-                        selectedValues={this.state.selectedValue} // Preselected value to persist in dropdown
-                        onSelect={this.onSelect} // Function will trigger on select event
-                        onRemove={this.onRemove} // Function will trigger on remove event
-                        displayValue="name" // Property name to display in the dropdown options
-                    />
-                </div>*/}
                 <div className={s.container}>
-                    {this.state.filterValue.value === 'All Technology' || this.state.filterValue=== 'All Technology' ?
-                        this.state.components.map((components) => (
-                            <div className={s.item} key={components.id}>
-                                <Logo className={s.logo}/>
-                                <h2>{components.name}</h2>
-                                <p className={s.tags}>{components.tagImport}&nbsp;&nbsp;&nbsp;{components.tagStacks}</p>
-                            </div>
-                        )) :
-                        this.state.components.filter(components => components.tagStacks === this.state.filterValue.value).map((components) => (
-                            <div className={s.item} key={components.id}>
-                                <Logo className={s.logo}/>
-                                <h2>{components.name}</h2>
-                                <div>
+                    {this.state.filterValue.map((tag) => (
+                        tag === [] || tag === 'All' ?
+                            this.state.components.map((components) => (
+                                <div className={s.item} key={components.id}>
+                                    <Checkbox className={s.logo}/>
+                                    <h2>{components.name}</h2>
                                     <p className={s.tags}>{components.tagImport}&nbsp;&nbsp;&nbsp;{components.tagStacks}</p>
                                 </div>
-                            </div>
-                        ))
-                    }
+                            ))
+                        :
+                            this.state.components.filter(components => components.tagStacks === tag || components.tagImport === tag).map((components) => (
+                                <div className={s.item} key={components.id}>
+                                    <Checkbox className={s.logo}/>
+                                    <h2>{components.name}</h2>
+                                    <div>
+                                        <p className={s.tags}>{components.tagImport}&nbsp;&nbsp;&nbsp;{components.tagStacks}</p>
+                                    </div>
+                                </div>
+                            ))
+                    ))}
                 </div>
             </div>
         );
