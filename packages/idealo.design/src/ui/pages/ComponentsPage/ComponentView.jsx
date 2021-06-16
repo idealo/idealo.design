0ed6ba0246f53de55e309ea6736f48666d71b0e8
Lucia Feddersen <lucia.feddersen@w0epx.fritz.box>
2021-06-16 17:44:45 +0200

    working on filtering

--- packages/idealo.design/src/ui/pages/ComponentsPage/ComponentView.jsx
+++ packages/idealo.design/src/ui/pages/ComponentsPage/ComponentView.jsx
@@ -14,6 +14,7 @@ class ComponentView extends React.Component {
             filterValue: ['All'],
             components: [],
             options: [],
+            filteredComponents: []
         }
     }
 
@@ -21,11 +22,11 @@ class ComponentView extends React.Component {
         this.setState({components : await fetchMap(), options: await fetchTags()})
         this.fillComponents();
         this.fillOptions();
-        this.getTheTags();
+        this.fillFilterComponents();
     }
 
     fillOptions(){
-        const optionsForFill = [{label: "all" , value: 'all'}]
+        const optionsForFill = []
         for(let i=0; i<this.state.options.length; i++){
             optionsForFill.push({label: this.state.options[i].tag_name, value: this.state.options[i].tag_name})
         }
@@ -34,43 +35,70 @@ class ComponentView extends React.Component {
         })
     }
 
+    fillFilterComponents(){
+        if(this.state.filteredComponents.length < 1) {
+            this.setState({filteredComponents: this.state.components})
+        }
+        else{
+            this.setState({filteredComponents:[]})
+            const filteredComponents = [];
+            for(let i=0; i<this.state.components.length; i++){
+                for(let j=0; j<this.state.filterValue.length; j++) {
+                    for(let x=0; x<this.state.components[i].tags.length; x++){
+                        if(this.state.components[i].tags[x] === this.state.filterValue[j]){
+                            filteredComponents.push(this.state.components[i])
+                        }
+                    }
+                }
+            }
+            filteredComponents.filter(function(ele , pos){return filteredComponents.indexOf(ele) === pos;});
+            this.setState({filteredComponents: filteredComponents})
+        }
+    }
+
     fillComponents(){
         const componentsForFill = []
         const tags = []
         for(let i=0; i<this.state.components.length; i++)
         {
-
             for(let j=i+1; j<this.state.components.length; j++){
                 if(this.state.components[i].component_id === this.state.components[j].component_id){
                     tags.push(this.state.components[i].tag_name);
                     tags.push(this.state.components[j].tag_name);
-                    var tagString = JSON.stringify(tags)
-                    console.log('😇😇🚴‍♀', this.state.components[i].component_id, this.state.components[i].title, tagString);
-                    console.log('parse', JSON.parse(tagString))
-                    componentsForFill.push({id: this.state.components[i].component_id, title : this.state.components[i].title, tags: JSON.parse(tagString) })
+                    componentsForFill.push({id: this.state.components[i].component_id, title : this.state.components[i].title, tags: JSON.parse(JSON.stringify(tags)) })
                 }
             }
             while(tags.length > 0) {
                 tags.pop();
             }
         }
-        console.log('idee 🐶', componentsForFill);
         this.setState({components : componentsForFill})
     }
+/*
 
-    getTheTags(){
-        this.state.components.map((c) => (
-            c.tags.map((t) =>
-                console.log('c',t)
-            )
-        ))
+    getTheTags() {
+        console.log('filterValueTag',filterValueTag)
+        for (let j = 0; j < this.state.components.length; j++) {
+            console.log('this.state.components.tags[j].length',this.state.components.tags[j].length)
+            /!*for (let i = 0; i < this.state.components.tags[j].length; i++) {
+                console.log('this.state.components.tags[i]',this.state.components.tags[i]);
+                if (this.state.components.tags[i] === filterValueTag) {
+                    return true;
+                }
+            }*!/
+        }
+        return '#react'
     }
+*/
+
+
 
 
     setFilter(select) {
         const a = [];
         select.map((select) => (a.push(select.value)))
         this.setState( this.state.filterValue = a);
+        this.fillFilterComponents();
     }
 
     render() {
@@ -79,37 +107,48 @@ class ComponentView extends React.Component {
                 <div className={s.multiselect}>
                         <Select
                             isMulti
-                            defaultValue={{ label: 'All', value: 'All'}}
+                            //defaultValue={{ label: 'All', value: 'All'}}
                             options={this.state.options}
                             className="basic-multi-select"
                             onChange={(value) => this.setFilter(value)}
                             classNamePrefix="select"
                         />
                     </div>
-                {/*<div className={s.container}>
+                <div className={s.container}>
+                    {this.state.filteredComponents.map((component) =>(
+                        <div className={s.item} key={component.id}>
+                            <Checkbox className={s.logo}/>
+                            <h1>{component.title}</h1>
+                            <h3 className={s.tags} >{component.tags}</h3>
+                        </div>
+                    ))}
+
+                </div>
+               {/* <div className={s.container}>
 
                         {this.state.filterValue.map((tag) => (
-                            tag === [] || tag === 'All' ?
-                                this.state.components.map((components) => (
-                                    <div className={s.item} key={components.id}>
+                            tag === 'All' ?
+                                this.state.components.map((component) => (
+                                    <div className={s.item} key={component.id}>
                                         <Checkbox className={s.logo}/>
-                                        <h2>{components.name}</h2>
-                                        <p className={s.tags}>{components.tagImport}&nbsp;&nbsp;&nbsp;{components.tagStacks}</p>
+                                        <h1>{component.title}</h1>
+                                        <h3 className={s.tags} >{component.tags}</h3>
                                     </div>
                             ))
                             :
-                            this.state.components.filter(components => components.tagStacks === tag || components.tagImport === tag).map((components) => (
-                                <div className={s.item} key={components.id}>
-                                    <Checkbox className={s.logo}/>
-                                    <h2>{components.name}</h2>
-                                    <div>
-                                        <p className={s.tags}>{components.tagImport}&nbsp;&nbsp;&nbsp;{components.tagStacks}</p>
+
+                            this.state.components.filter(tag === this.getTheTags()).map((component) => (
+                                <div className={s.item} key={component.id}>
+                                    <div className={s.item} key={component.id}>
+                                        <Checkbox className={s.logo}/>
+                                        <h1>{component.title}</h1>
+                                        <h3 className={s.tags} >{component.tags}</h3>
                                     </div>
                                 </div>
                             ))
                         ))}
                     </div>*/}
-                    <div className={s.container}>
+                    {/*<div className={s.container}>
                         {this.state.components.map((components) => (
                             <div className={s.item} key={components.id}>
                                 <Checkbox className={s.logo}/>
@@ -118,7 +157,7 @@ class ComponentView extends React.Component {
                             </div>
                             ))
                         }
-                    </div>
+                    </div>*/}
             </div>
         );
     }
