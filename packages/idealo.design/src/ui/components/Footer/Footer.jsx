6f31bd3e0f1f4656b06ddc8d275a4a3a3c248973
MuellerBettina <MuellerBettina@pm.me>
2021-06-24 16:32:25 +0200

    added facebook and twitter link

--- packages/idealo.design/src/ui/components/Footer/Footer.jsx
+++ packages/idealo.design/src/ui/components/Footer/Footer.jsx
@@ -10,9 +10,16 @@ class Footer extends React.Component {
     render() {
         return(
             <footer className={s.Footer}>
-                <div className="footer">
-                    <a href="https://www.idealo.co.uk/terms.html">About/Terms & Conditions</a>
+                <div className="footer-links">
+                    <a href="https://www.idealo.co.uk/terms.html">Privacy</a>
+                    <a href="https://www.facebook.com/idealo/"><img alt="" src="https://img.icons8.com/dusk/64/000000/facebook.png"/>Facebook</a>
+
+                    <a href="https://www.idealo.co.uk/privacypolicy.html">About/Terms & Conditions</a>
                     <p>This is some content in sticky footer</p>
+                    //put links for social media here
+                    <a href="https://www.facebook.com/idealo/"><img alt="" src="https://img.icons8.com/dusk/64/000000/facebook.png"/>Facebook</a>
+                    <a href="https://twitter.com/idealo_uk"><img alt="" src="https://img.icons8.com/doodle/48/000000/twitter--v1.png"/>twitter</a>
+
                 </div>
             </footer>
         )
