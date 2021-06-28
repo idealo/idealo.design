262c75167e67608ac542b414e65453247aed94db
MuellerBettina <MuellerBettina@pm.me>
2021-06-28 11:46:10 +0200

    fixed instagram link

--- packages/idealo.design/src/ui/components/Footer/Footer.jsx
+++ packages/idealo.design/src/ui/components/Footer/Footer.jsx
@@ -16,7 +16,7 @@ class Footer extends React.Component {
                 </div>
                 <div className={s.SocialMedia}>
                     <a href="https://www.facebook.com/idealo/"><img src="https://www.vectorico.com/wp-content/uploads/2018/02/Facebook-Icon-Dark.png" alt="Facebook Logo" height={40} width={40}/></a>
-                    <a href="https://twitter.com/idealo_uk"><img src="https://www.vectorico.com/wp-content/uploads/2018/02/Instagram-Icon.png" alt="Instagram Logo"height={40} width={40}/></a>
+                    <a href="https://www.instagram.com/idealo/?hl=en"><img src="https://www.vectorico.com/wp-content/uploads/2018/02/Instagram-Icon.png" alt="Instagram Logo"height={40} width={40}/></a>
                     <a href="https://twitter.com/idealo_uk"><img src="https://www.vectorico.com/wp-content/uploads/2018/02/Twitter-Social-Icon.png" alt="Twitter Logo" height={40} width={40}/></a>
                 </div>
             </footer>
