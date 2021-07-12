3f1da49602e3d4fb812a0598a3ffea6b55aef7ba
iuliiaromanova <romanovajb@gmail.com>
2021-07-12 11:30:36 +0200

    write test for ComponentView filter

--- packages/idealo.design/src/__tests__/componentView.test.jsx
+++ packages/idealo.design/src/__tests__/componentView.test.jsx
@@ -15,7 +15,7 @@ jest.mock('../ui/pages/ComponentsPage/component_data', () => {
 
 test('ComponentView gets rendered with user logged in', async () => {
     const mockupComponent = [{
-        "component_id": 123,
+        "component_id": 1,
         "title": "@motif/button",
     }]
 
@@ -27,9 +27,9 @@ test('ComponentView gets rendered with user logged in', async () => {
     ]
 
     const mockupMap = [{
-        "component_id": 123,
+        "component_id": 1,
         "title": "@motif/button",
-        "tag_name": "motif"
+        "tag_name": "button"
     }]
 
     const userInfo = {
@@ -65,4 +65,73 @@ test('ComponentView gets rendered with user logged in', async () => {
         expect(componentTags).toBeInTheDocument()
     })
 
+})
+
+test('ComponentView filter', async () => {
+    const mockupComponent = [{
+        "component_id": 1,
+        "title": "@motif/button",
+    }, {
+        "component_id": 2,
+        "title": "@motif/pl-button",
+    }, {
+        "component_id": 3,
+        "title": "@motif/checkbox",
+    }]
+
+    const mockupTags = [
+        {"tag_name": "motif"},
+        {"tag_name": "motif-ui"},
+        {"tag_name": "button"},
+        {"tag_name": "checkbox"}
+    ]
+
+    const mockupMap = [{
+        "component_id": 1,
+        "title": "@motif/button",
+        "tag_name": "button"
+    }, {
+        "component_id": 2,
+        "title": "@motif/pl-button",
+        "tag_name": "button"
+    }, {
+        "component_id": 3,
+        "title": "@motif/checkbox",
+        "tag_name": "checkbox"
+    }]
+
+    const userInfo = {
+        "status": "LOGGED_IN",
+        "user": {
+            "@odata.context": null,
+            "businessPhones": [],
+            "displayName": null,
+            "givenName": null,
+            "jobTitle": null,
+            "mail": null,
+            "mobilePhone": null,
+            "officeLocation": null,
+            "preferredLanguage": null,
+            "surname": null,
+            "userPrincipalName": null,
+            "id": null
+        }
+    };
+
+    fetchUserInfo.mockReturnValue(userInfo)
+    fetchComponents.mockReturnValue(mockupComponent)
+    fetchTags.mockReturnValue(mockupTags)
+    fetchMap.mockReturnValue(mockupMap)
+
+
+    delete window.location
+    window.location = new URL('http://localhost:8080/components?query=button')
+
+
+    render(<ComponentView/>)
+
+    await waitFor(() => {
+        const componentTag = screen.getAllByTitle('componentTitle')
+        expect(componentTag).toHaveLength(2)
+    })
 })
