ecdba9d01a9c23a9b907d63c0259b4e746be8ea2
svenjakrauss <s0568990@htw-berlin.de>
2021-07-23 18:31:39 +0200

    solve problem with componentDidUpdate

--- packages/idealo.design/src/ui/pages/ComponentsPage/ComponentsDetailView.jsx
+++ packages/idealo.design/src/ui/pages/ComponentsPage/ComponentsDetailView.jsx
@@ -3,21 +3,19 @@ import React from 'react'
 import s from './ComponentsPage.module.scss'
 
 import {withRouter} from "react-router";
-import {fetchSingleComponent, fetchReadMe} from "./component_data";
-import slugify from "slugify";
-import {fetchSinglePost} from "../BlogPage/data";
+import {fetchSingleComponent} from "./component_data";
 
 export class ComponentsDetailView extends React.Component {
 
     constructor(props) {
         super(props);
-        this.handleClick = this.handleClick.bind(this);
-        const {history} = props;
+        this.showInstallation = this.showInstallation.bind(this);
+        this.showUsage = this.showUsage.bind(this);
+        this.updateComponentDetailView = this.updateComponentDetailView.bind(this)
+
         this.state = {
             slug: '',
-            history: history,
             component: {},
-            readme: [],
             links: [],
             URLOptions: '',
             result: ''
@@ -29,64 +27,48 @@ export class ComponentsDetailView extends React.Component {
         if (slug) {
             this.setState({
                 component: await fetchSingleComponent({slug}),
-                readme: await fetchReadMe({slug}),
                 slug: slug,
                 links: ['Design','Installation', 'Usage', 'Story Source', 'Prop Types']
             })
-        }
-    }
-
-    async componentDidUpdate(prevProps) {
-        /*if (prevProps.match.params.slug !== this.props.match.params.slug) {
-            this.setState({
-                component: await fetchSingleComponent({slug: this.props.match.params.slug}),
-                readme: await fetchReadMe({slug: this.props.match.params.slug}),
-                slug: this.props.match.params.slug,
-                links: ['Design','Installation', 'Usage', 'Story Source', 'Prop Types'],
-            })
-        }*/
-        this.checkURL()
-    }
-
-    handleClick() {
-        //console.log('🥞')
-        this.checkURL();
-        //console.log('hsbjsadbksadbkjw')
-    }
-
-    checkURL() {
-        const URLOptions = [];
-        const url = slugify(window.location.href.toString());
-
-        for (let i = 0; i < this.state.links.length; i++) {
-            if (url.includes(this.state.links[i])) {
-                URLOptions.push(this.state.links[i])
+            if(window.location.href.includes('#')){
+                await this.updateComponentDetailView()
             }
         }
-        this.setState({URLOptions: URLOptions})
-        //console.log('kommt er hier hin')
-        this.fillComponentsWithReadMe()
     }
 
-    fillComponentsWithReadMe(){
-        for(let i=0; i<this.state.readme.length; i++) {
-            if (this.state.readme[i].slug === this.state.slug) {
-                const a = this.state.readme[i].readme.content
-                const use = a[Object.keys(a)[0]]
-                const inst = a[Object.keys(a)[1]]
-                const insta = inst[Object.keys(inst)[0]]
-                const usage = use[Object.keys(use)[0]]
-                const header1 = JSON.stringify(inst[Object.keys(inst)[1]])
-                const header2 = JSON.stringify(use[Object.keys(use)[1]])
-                //console.log('kurz for if else in fillcomponentswithreadme')
-                if(header1.includes(this.state.URLOptions[0])){
-                    this.setState({result: insta})
-                }
-                else if(header2.includes(this.state.URLOptions[0])){
-                    this.setState({result: usage})
-                }
-            }
+    async componentDidUpdate(prevProps, prevState, snapshot) {
+        if(window.location.href.includes('#')){
+            await this.updateComponentDetailView()
         }
+    }
+
+    async updateComponentDetailView(){
+        try{
+            const slug = window.location.href
+            if(slug.includes('Installation')){
+                this.showInstallation()
+            }else if(slug.includes('Usage')){
+                this.showUsage()
+            }
+        }catch(e){
+
+        }
+    }
+
+    showInstallation(){
+        const allReadmeContent = this.state.component.readme.content
+        const inst = allReadmeContent[Object.keys(allReadmeContent)[1]]
+        const insta = inst[Object.keys(inst)[0]]
+
+        this.setState({result: insta})
+    }
+
+    showUsage(){
+        const allReadmeContent = this.state.component.readme.content
+        const use = allReadmeContent[Object.keys(allReadmeContent)[0]]
+        const usage = use[Object.keys(use)[0]]
+
+        this.setState({result: usage})
     }
 
     render() {
@@ -97,8 +79,8 @@ export class ComponentsDetailView extends React.Component {
                     <p>----------------------------</p>
                     <ul>
                         <li><a href='#Design'>Design</a></li>
-                        <li><a href="#Installation" onClick={() => this.handleClick}>Installation</a></li>
-                        <li><a href="#Usage" onClick={() => this.handleClick}>Usage</a></li>
+                        <li><a href="#Installation">Installation</a></li>
+                        <li><a href="#Usage">Usage</a></li>
                         <li><a href="#Story Source">Story Source</a></li>
                         <li><a href="#Prop Types">Prop Types</a></li>
                     </ul>
@@ -114,4 +96,3 @@ export class ComponentsDetailView extends React.Component {
 }
 
 export default withRouter(ComponentsDetailView);
-
