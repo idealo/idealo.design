68f36450e5625c7fa6ff48abf835fc4aa0c2d0f8
Lucia Feddersen <lucia.feddersen@w0epx.fritz.box>
2021-08-04 17:52:29 +0200

    get the contetent from the figma api

--- packages/idealo.design/scripts/figmaImporter.js
+++ packages/idealo.design/scripts/figmaImporter.js
@@ -3,29 +3,47 @@ import fetch from 'node-fetch';
 
 let importFromApiFigma = fetch('https://api.figma.com/v1/files/ybZtLgPiNd2hUWlS2W80DS/', {
     headers: {
-        "X-Figma-Token": " --- "
+        "X-Figma-Token": "218206-02837e42-f682-4cb5-aab6-731b7548b06e"
     }
 })
     .then(response => response.json())
     .then((data) => {
-        getTheIdsfromFigmaApi(data)
+        getComponentsFromFigmaApi(data)
     });
 
-function getTheIdsfromFigmaApi(data){
-    const titles = [];
+function getComponentsFromFigmaApi(data){
+    let components = [];
     for(const childComponent of data.document.children){
-        titles.push(childComponent.name)
+        components.push({'title': childComponent.name , content: []})
         for(const childFrames of childComponent.children ){
             if(childFrames.name.includes('USAGE')){
+                let titlesContent = []
+                let contents = []
                 for(const childContent of childFrames.children){
                     if(childContent.characters !== undefined){
-                        console.log('🎟name:',childContent.name)
-                        console.log('🏉characters',childContent.characters)
-                        console.log('🚣‍♂️ganzer inhalt', childContent)
+                        if(childContent.style.textAutoResize === 'WIDTH_AND_HEIGHT'){
+                            titlesContent.push({'content': childContent.characters , "x": childContent.absoluteBoundingBox.x, "y": childContent.absoluteBoundingBox.y})
+                        }
+                        else{
+                            contents.push({content:childContent.characters , x: childContent.absoluteBoundingBox.x, y: childContent.absoluteBoundingBox.y})
+                        }
+                    }
+                }
+
+               for(const titleContent of titlesContent){
+                    for(const content of contents){
+                        if(titleContent.x === content.x && titleContent.y <= content.y && titleContent.y >= content.y-180){
+                            for(const component of components){
+                                if(component.title === childComponent.name){
+                                    component.content.push({'titleContent': titleContent.content, 'content': content.content})
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
     }
+    console.log(components)
 }
 
