f1f52859677b864b98bf02b12ef7bdb96c431b6d
svenjakrauss <s0568990@htw-berlin.de>
2021-08-27 18:00:33 +0200

    adapt code of figma importer to usage-groups

--- scripts/figmaImporter.js
+++ scripts/figmaImporter.js
@@ -18,36 +18,32 @@ let importFromApiFigma = fetch('https://api.figma.com/v1/files/ybZtLgPiNd2hUWlS2
 async function getComponentsFromFigmaApi(data){
     let components = [];
     for(const childComponent of data.document.children){
-        components.push({'title': childComponent.name , content: []})
+        let component = {}
+        component.title = childComponent.name
+        component.content = []
         for(const childFrames of childComponent.children ){
             if(childFrames.name.includes('USAGE')){
-                let titlesContent = []
-                let contents = []
-                for(const childContent of childFrames.children){
-                    if(childContent.characters !== undefined){
-                        if(childContent.style.textAutoResize === 'WIDTH_AND_HEIGHT' && childContent.style.fontSize >20){
-                            titlesContent.push({'content': childContent.characters , "x": childContent.absoluteBoundingBox.x, "y": childContent.absoluteBoundingBox.y})
-                        }
-                        else{
-                            contents.push({content:childContent.characters , x: childContent.absoluteBoundingBox.x, y: childContent.absoluteBoundingBox.y})
-                        }
-                    }
-                }
-
-               for(const titleContent of titlesContent){
-                    for(const content of contents){
-                        if((Math.abs(titleContent.x - content.x)<2) && titleContent.y <= content.y && ((content.y - titleContent.y) <100)){
-                            for(const component of components){
-                                if(component.title === childComponent.name){
-                                    component.content.push({'titleContent': titleContent.content, 'content': content.content})
+                let content = []
+                for(const childContent of childFrames.children.reverse()){
+                    if(childContent.type === "GROUP"){
+                        for(const child of childContent.children){
+                            if(child.characters !== undefined){
+                                if(child.style.fontSize>18){
+                                    content.push({titleContent: child.characters})
+                                }else{
+                                    content.push({content: child.characters})
                                 }
+                                console.log(content)
                             }
                         }
                     }
                 }
+                component.content = content
             }
         }
+        components.push(component)
     }
+    console.log(components)
     return components
 }
 
@@ -55,6 +51,7 @@ async function sendDataToHttpRequest(components) {
     const username = process.env.UPLOADER_USERNAME
     const password = process.env.UPLOADER_PWD
     for (const component of components){
+        //console.log('c',component)
         await axios.put(process.env.BASE_URL + '/api/components/import/figma', component, {
             headers: {
                 ...component,
