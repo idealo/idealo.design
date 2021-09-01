aba81325f719bac3d01cb5d855fb83326f4b2764
AJ <42003122+AJ-7885@users.noreply.github.com>
2021-09-01 20:43:26 +0200

    rename Design-System to ids-react

--- src/ui/pages/AtomsPage/AtomsPage.jsx
+++ src/ui/pages/AtomsPage/AtomsPage.jsx
@@ -6,7 +6,7 @@ export default function AtomsPage(props) {
         <>
             <h1>Atoms</h1>
             <div>
-                <a target="_blank" href="https://github.com/idealo/design-system/tree/main/src/components/atoms">design-system/atoms on Github</a>
+                <a target="_blank" href="https://github.com/idealo/ids-react/tree/main/src/components/atoms">iDS-react/atoms on Github</a>
             </div>
             <i>Internal Github project</i>
         </>
--- src/ui/pages/MoleculesPage/MoleculesPage.jsx
+++ src/ui/pages/MoleculesPage/MoleculesPage.jsx
@@ -6,7 +6,7 @@ export default function MoleculesPage(props) {
         <>
             <h1>Molecules</h1>
             <div>
-                <a target="_blank" href="https://github.com/idealo/design-system/tree/main/src/components/molecules">design-system/molecules on Github</a>
+                <a target="_blank" href="https://github.com/idealo/ids-react/tree/main/src/components/molecules">iDS-react/molecules on Github</a>
             </div>
             <i>Internal Github project</i>
         </>
