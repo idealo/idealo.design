57782cdae75d14d496c37a7d2e7b9b5a8e801c4d
svenjakrauss <s0568990@htw-berlin.de>
2021-09-09 11:33:25 +0200

    change sql-syntax of db-migration-file

--- src/server/db_Configuration/11-db.sql
+++ src/server/db_Configuration/11-db.sql
@@ -7,11 +7,8 @@ alter table blogposts alter column previouspost type integer using previouspost:
 alter table blogposts alter column nextpost type integer using nextpost::integer;
 
 UPDATE blogposts SET previouspost = 2 where id=1;
-UPDATE blogposts SET previouspost = 3 and nextpost = 1 where id=2;
-UPDATE blogposts SET previouspost = 4 and nextpost = 2 where id=3;
-UPDATE blogposts SET previouspost = 5 and nextpost = 3 where id=4;
-UPDATE blogposts SET previouspost = 6 and nextpost = 4 where id=5;
+UPDATE blogposts SET previouspost = 3, nextpost = 1 where id=2;
+UPDATE blogposts SET previouspost = 4, nextpost = 2 where id=3;
+UPDATE blogposts SET previouspost = 5, nextpost = 3 where id=4;
+UPDATE blogposts SET previouspost = 6, nextpost = 4 where id=5;
 UPDATE blogposts SET nextpost = 5 where id=6;
-
-
-
