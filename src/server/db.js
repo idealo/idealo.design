01ac15b7ea91ce2d626a5bd3c24e7c6c0405243c
svenjakrauss <s0568990@htw-berlin.de>
2021-09-14 12:04:11 +0200

    write db functions for insert, update and delete single blogpost

--- src/server/db.js
+++ src/server/db.js
@@ -2,7 +2,13 @@ import {Sequelize, Model, DataTypes, Op} from 'sequelize'
 
 const sequelize = new Sequelize (
     process.env.POSTGRES_URL ||
-    "postgres://database-idealo-design.c9fyhsob8bxc.eu-central-1.rds.amazonaws.com"
+    "postgres://database-idealo-design.c9fyhsob8bxc.eu-central-1.rds.amazonaws.com",
+    {
+      dialectOptions: {
+        useUTC: false
+      },
+      timezone: '+02:00'
+    }
 );
 
 (async () => {
@@ -77,7 +83,7 @@ export class Blog extends Model {
             }
           })
         },
-        ta
+        transaction: ta
       })
 
       let nextSlug = await Blog.findAll({
@@ -96,7 +102,7 @@ export class Blog extends Model {
             }
           })
         },
-        ta
+        transaction: ta
       })
 
       if (nextSlug.length === 0) {
@@ -106,12 +112,185 @@ export class Blog extends Model {
       }
       if (prevSlug.length === 0) {
         prevSlug = {slug: null}
-      }else if(prevSlug.length===1){
+      }else if(prevSlug.length === 1){
         prevSlug = prevSlug[0]
       }
       await ta.commit()
       return [prevSlug, nextSlug]
     }catch (e){
+      await ta.rollback()
+    }
+  }
+
+  static async insertSingleBlogpost({
+      title = "",
+      date,
+      categoryDisplayValue = "",
+      categorySlug = "",
+      slug,
+      image = "",
+      blogpostcontent,
+      isArchived = 0
+                              }){
+    const ta = await sequelize.transaction()
+    try {
+      const newBlogpost = await Blog.create({
+        title: title,
+        date: date,
+        categorydisplayvalue: categoryDisplayValue,
+        categoryslug: categorySlug,
+        slug: slug,
+        image: image,
+        blogpostcontent: blogpostcontent,
+        isarchived: isArchived,
+        nextpost: await Blog.findOne({
+          attributes: ['id'],
+          where: {
+            isarchived: 0,
+            date: await Blog.findOne({
+              attributes: [[sequelize.fn('max', sequelize.col('date')), 'date']],
+              where: {
+                isarchived: 0
+              },
+              transaction: ta
+            }).then(date => {
+              return date.dataValues.date
+            })
+          },
+          transaction: ta
+        }).then(id => {
+          return id.dataValues.id
+        })
+      })
+
+      const updateNextPost = await Blog.update({
+            previouspost: await Blog.findOne({
+              attributes: ['id'],
+              where: {
+                slug: slug
+              },
+              transaction: ta
+            }).then(id => {
+              return id.dataValues.id
+            }),
+          },
+          {
+            where: {
+              isarchived: 0,
+              date: await Blog.findOne({
+                attributes: [[sequelize.fn('max', sequelize.col('date')), 'date']],
+                where: {
+                  [Op.and]: [
+                    {isarchived: 0},
+                    {
+                      date: {
+                        [Op.lt]: await Blog.findOne({
+                          attributes: [[sequelize.fn('max', sequelize.col('date')), 'date']],
+                          transaction: ta
+                        }).then(date => {
+                          return date.dataValues.date
+                        })
+                      }
+                    }
+                  ]
+                },
+                transaction: ta
+              }).then(date => {
+                return date.dataValues.date
+              })
+            }, transaction: ta
+          })
+      await ta.commit()
+    }catch (e) {
+      console.error(e)
+      await ta.rollback()
+    }
+  }
+
+  static updateBlogpost(blog){
+    return Blog.update({
+      title: blog.title,
+      slug: blog.slug,
+      categorydisplayvalue: blog.categorydisplayvalue,
+      categoryslug: blog.categoryslug,
+      blogpostcontent: JSON.parse(blog.blogpostcontent)
+    }, {
+      where: {
+        id: blog.id
+      }
+    })
+  }
+
+  static async deleteSingleBlogpost(blog) {
+    const ta = await sequelize.transaction()
+    try {
+      if (blog.previouspost === null && blog.nextpost) {
+        await Blog.update({
+          previouspost: null
+        }, {
+          where: {
+            previouspost: blog.id
+          },
+          transaction: ta
+        })
+      } else if (blog.nextpost === null && blog.previouspost) {
+        await Blog.update({
+          nextpost: null
+        }, {
+          where: {
+            nextpost: blog.id
+          },
+          transaction: ta
+        })
+      }
+
+      if (blog.previouspost === null) {
+        await Blog.update({
+          previouspost: null
+        }, {
+          where: {
+            previouspost: blog.id
+          },
+          transaction: ta
+        })
+      } else if (blog.nextpost === null) {
+        await Blog.update({
+          nextpost: null
+        }, {
+          where: {
+            nextpost: blog.id
+          },
+          transaction: ta
+        })
+      } else if (blog.nextpost && blog.previouspost) {
+        await Blog.update({
+          previouspost: blog.previouspost
+        }, {
+          where: {
+            previouspost: blog.id
+          },
+          transaction: ta
+        })
+
+        await Blog.update({
+          nextpost: blog.nextpost
+        }, {
+          where: {
+            nextpost: blog.id
+          },
+          transaction: ta
+        })
+      }
+      await Blog.destroy({
+        where: {
+          id: blog.id
+        },
+        transaction: ta
+      })
+
+      await ta.commit()
+    }catch (e) {
+      console.error(e)
       await ta.rollback()
     }
   }
