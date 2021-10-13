4fb802816b0bef6c9b912b520203d8c82a88e776
svenjakrauss <s0568990@htw-berlin.de>
2021-10-13 19:37:52 +0200

    fix bug that error is shown for a short time at the beginning of loading the page

--- src/ui/pages/BlogPage/DetailView.jsx
+++ src/ui/pages/BlogPage/DetailView.jsx
@@ -18,51 +18,73 @@ import ErrorPage from "../../components/ErrorPage/ErrorPage";
 export class DetailView extends React.Component {
   constructor(props) {
     super(props);
-    const { history } = props;
-
     this.state = {
-      history: history,
+      isBlogpostLoaded: false,
+    }
+  }
+
+
+  async componentDidMount() {
+    const blogpost = await fetchSinglePost({slug: this.props.match.params.slug})
+    if(blogpost){
+      this.setState({
+        isBlogpostLoaded: true,
+      })
+    }
+  }
+
+  render() {
+    const isRendered = false
+    return !isRendered || this.state.isBlogpostLoaded ?
+        <Blogpost {...this.props}/> :
+        <Error/>
+  }
+}
+
+class Blogpost extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
       userInfo: [],
       blogpost: {},
       slug: null,
       slugPreviouspost: null,
       slugNextpost: null,
       isPromptOpen: false,
-    };
+    }
 
-    this.handlePostEdit = this.handlePostEdit.bind(this);
-    this.goBack = this.goBack.bind(this);
-    this.handlePopup = this.handlePopup.bind(this);
     this.handleDeletion = this.handleDeletion.bind(this);
     this.handleArchive = this.handleArchive.bind(this);
+    this.handlePostEdit = this.handlePostEdit.bind(this);
+    this.handlePopup = this.handlePopup.bind(this);
     this.onModalLeave = this.onModalLeave.bind(this);
     this.convertToHtml = this.convertToHtml.bind(this);
     this.convertDatetime = this.convertDatetime.bind(this);
+    this.goBack = this.goBack.bind(this);
+
   }
 
   async componentDidMount() {
+    this.setState({
+      userInfo: await fetchUserInfo(),
+    });
     const slug = this.props.match.params.slug;
     if (slug) {
       this.setState({
         blogpost: await fetchSinglePost({ slug: slug }),
-        slug: slug,
       });
-    }
-    this.setState({
-      userInfo: await fetchUserInfo(),
-    });
-
-    if(this.state.blogpost){
-      const slugsPreviousAndNextPost = await fetchPrevSlugAndNextSlugById({id: this.state.blogpost.id })
-      if(slugsPreviousAndNextPost[0]!==null){
-        this.setState({
-          slugPreviouspost: slugsPreviousAndNextPost[0].slug,
-        })
-      }
-      if(slugsPreviousAndNextPost[1]!==null){
-        this.setState({
-          slugNextpost: slugsPreviousAndNextPost[1].slug,
-        })
+      if(this.state.blogpost){
+        const slugsPreviousAndNextPost = await fetchPrevSlugAndNextSlugById({id: this.state.blogpost.id })
+        if(slugsPreviousAndNextPost[0]!==null){
+          this.setState({
+            slugPreviouspost: slugsPreviousAndNextPost[0].slug,
+          })
+        }
+        if(slugsPreviousAndNextPost[1]!==null){
+          this.setState({
+            slugNextpost: slugsPreviousAndNextPost[1].slug,
+          })
+        }
       }
     }
   }
@@ -71,25 +93,32 @@ export class DetailView extends React.Component {
     if (prevProps.match.params.slug !== this.props.match.params.slug) {
       this.setState({
         blogpost: await fetchSinglePost({ slug: this.props.match.params.slug }),
-        slug: this.props.match.params.slug,
       });
     }
   }
 
+  goBack() {
+    this.props.history.push({
+      pathname: `/blog`,
+    });
+  }
+
   toDateFormat_de(inp) {
-    let date = inp ? new Date(inp) : new Date();
+    if(inp){
+      let date = inp ? new Date(inp) : new Date();
 
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    const hour = date.getHours();
-    const minute = String(date.getMinutes()).padStart(2, "0");
+      const year = date.getFullYear();
+      const month = date.getMonth() + 1;
+      const day = date.getDate();
+      const hour = date.getHours();
+      const minute = String(date.getMinutes()).padStart(2, "0");
 
-    return `${day}.${month}.${year} um ${hour}:${minute} Uhr`;
+      return `${day}.${month}.${year} um ${hour}:${minute} Uhr`;
+    }
   }
 
   handlePostEdit() {
-    this.state.history.push({
+    this.props.history.push({
       pathname: `/blog/${this.state.slug}/edit`,
       search: `?slug=${this.state.slug}`,
     });
@@ -99,27 +128,9 @@ export class DetailView extends React.Component {
     document.body.scrollTop = 0;
   }
 
-  goBack() {
-    this.state.history.push({
-      pathname: `/blog/`,
-    });
-  }
-
-  handleDeletion() {
-    deleteSinglePost(this.state.blogpost).then(
-      this.state.history.push("/blog")
-    );
-  }
-
-  handleArchive() {
-    archiveSinglePost(this.state.blogpost).then(
-      this.state.history.push("/blog")
-    );
-  }
-
   onModalLeave() {
     this.setState({ isPromptOpen: false });
-    this.state.history.push(`/blog/${this.state.slug}`);
+    this.props.history.push(`/blog/${this.state.blogpost.slug}`);
   }
 
   handlePopup() {
@@ -137,77 +148,99 @@ export class DetailView extends React.Component {
     return this.toDateFormat_de(date);
   }
 
+  handleDeletion() {
+    deleteSinglePost(this.state.blogpost).then(
+        this.props.history.push("/blog")
+    );
+  }
+
+  handleArchive() {
+    archiveSinglePost(this.state.blogpost).then(
+        this.props.history.push("/blog")
+    );
+  }
+
   render() {
     return (
-      <div className={s.ContentBox}>
-        <div className={s.Menu}>
-          <button onClick={this.goBack}>Go Back</button>
-          {this.state.userInfo.status === "LOGGED_IN" && this.state.blogpost ? (
-            <button onClick={this.handlePopup} title="deleteButton">
-              Delete
-            </button>
-          ) : (
-            <div/>
-          )}
-          {this.state.userInfo.status === "LOGGED_IN" && this.state.blogpost ? (
-            <button onClick={this.handlePostEdit} title="editButton">
-              Edit
-            </button>
-          ) : (
-            <div/>
-          )}
-        </div>
+        <div className={s.ContentBox}>
+          <div className={s.Menu}>
+            <button onClick={this.goBack}>Go Back</button>
+            {this.state.userInfo.status === "LOGGED_IN" && this.state.blogpost ? (
+                  <button onClick={this.handlePopup} title="deleteButton">
+                    Delete
+                  </button>
+              ) : (
+                  <div/>
+              )}
+              {this.state.userInfo.status === "LOGGED_IN" && this.state.blogpost ? (
+                  <button onClick={this.handlePostEdit} title="editButton">
+                    Edit
+                  </button>
+              ) : (
+                  <div/>
+              )}
+            </div>
 
-        <div className={s.ContentDetailView}>
-          {!this.state.blogpost ? (
-              <ErrorPage errorCode = '404'/>
-          ) : (
-              <div>
-                <h2 className={s.blogpostTitle}>{this.state.blogpost.title}</h2>
-                <div className={s.Autor}>{this.state.blogpost.autor}</div>
-                <h5 className={s.blogpostDate}>{this.convertDatetime(this.state.blogpost.date)}</h5>
-                {this.convertToHtml(this.state.blogpost.blogpostcontent)}
-                <img
-                    aria-label="blogpostImage"
-                    alt=""
-                    src={this.state.blogpost.image}
-                />
+            <div className={s.ContentDetailView}>
+              {!this.state.blogpost ? (
+                  <ErrorPage errorCode = '404'/>
+              ) : (
+                  <div>
+                    <h2 className={s.blogpostTitle}>{this.state.blogpost.title}</h2>
+                    <div className={s.Autor}>{this.state.blogpost.autor}</div>
+                    <h5 className={s.blogpostDate}>{this.convertDatetime(this.state.blogpost.date)}</h5>
+                    {this.convertToHtml(this.state.blogpost.blogpostcontent)}
+                    <img
+                        aria-label="blogpostImage"
+                        alt=""
+                        src={this.state.blogpost.image}
+                    />
 
-                <div className={s.ButtonNavigation}>
-                  {this.state.blogpost.previouspost && (
-                      <a
-                          title="prevPost"
-                          href={`/blog/${this.state.slugPreviouspost}`}
-                          onClick={this.scrollToTop}
-                      >
-                        <button className={s.ButtonPrevious}>Previous</button>
-                      </a>
-                  )}
-                  {this.state.blogpost.nextpost && (
-                      <a
-                          title="nextPost"
-                          href={`/blog/${this.state.slugNextpost}`}
-                          onClick={this.scrollToTop}
-                          className={s.ButtonNext}
-                      >
-                        <button className={s.ButtonNextOnHover}>Next</button>
-                      </a>
-                  )}
-                </div>
-              </div>
-          )}
-        </div>
+                    <div className={s.ButtonNavigation}>
+                      {this.state.blogpost.previouspost && (
+                          <a
+                              title="prevPost"
+                              href={`/blog/${this.state.slugPreviouspost}`}
+                              onClick={this.scrollToTop}
+                          >
+                            <button className={s.ButtonPrevious}>Previous</button>
+                          </a>
+                      )}
+                      {this.state.blogpost.nextpost && (
+                          <a
+                              title="nextPost"
+                              href={`/blog/${this.state.slugNextpost}`}
+                              onClick={this.scrollToTop}
+                              className={s.ButtonNext}
+                          >
+                            <button className={s.ButtonNextOnHover}>Next</button>
+                          </a>
+                      )}
+                    </div>
+                  </div>
+              )}
+            </div>
 
-        <Prompt
-          show={this.state.isPromptOpen}
-          onDelete={this.handleDeletion}
-          onArchive={this.handleArchive}
-          onCancel={this.onModalLeave}
-          message="Do you want to delete or archive that post?"
-        />
-      </div>
-    );
+            <Prompt
+                show={this.state.isPromptOpen}
+                onDelete={this.handleDeletion}
+                onArchive={this.handleArchive}
+                onCancel={this.onModalLeave}
+                message="Do you want to delete or archive that post?"
+            />
+          </div>
+    )
   }
 }
 
-export default withRouter(DetailView);
+class Error extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+      return <ErrorPage errorCode = "404"/>
+  }
+}
+
+export default withRouter(DetailView)
