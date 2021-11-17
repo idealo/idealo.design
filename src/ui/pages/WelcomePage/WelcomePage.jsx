c677d6c8e2d27d16508ed4a87d135372f8e9fa49
Lara Kiyicioglu <lara-sumru.kiyicioglu@idealo.de>
2021-11-17 17:29:16 +0100

    page is now protected with login & first styled link

--- src/ui/pages/WelcomePage/WelcomePage.jsx
+++ src/ui/pages/WelcomePage/WelcomePage.jsx
@@ -3,6 +3,7 @@ import content from './content.json'
 import {withRouter} from "react-router";
 import {fetchUserInfo} from "../BlogPage/data";
 import LoginMessage from "../../components/LoginMessage/LoginMessage";
+import s from "./WelcomePage.module.scss";
 
 export class WelcomePage extends React.Component {
 
@@ -30,7 +31,7 @@ export class WelcomePage extends React.Component {
 
     render() {
         if(this.state.user){
-            return <WelcomePage {...this.props}/>
+            return <WelcomePageContent {...this.props}/>
         }else if(this.state.error){
             return <LoginMessage children="Library"/>
         }else{
@@ -39,18 +40,39 @@ export class WelcomePage extends React.Component {
     }
 }
 
-class Welcome extends React.Component {
-    constructor(props) {
-        super(props);
+function RenderElement(props) {
+    const elem = props.elem
+
+    switch (elem.type) {
+        case 'p':
+            return <p dangerouslySetInnerHTML={{__html: elem.content}}/>
+        case 'h1':
+            return <h1 dangerouslySetInnerHTML={{__html: elem.content}}/>
+        case 'h2':
+            return <h2 dangerouslySetInnerHTML={{__html: elem.content}}/>
+        case 'h3':
+            return <h3 dangerouslySetInnerHTML={{__html: elem.content}}/>
+        case 'h4':
+            return <h4 dangerouslySetInnerHTML={{__html: elem.content}}/>
+        case 'h5':
+            return <h5 dangerouslySetInnerHTML={{__html: elem.content}}/>
+        case 'link':
+            return <a className={s.a} href={elem.href}>{elem.name}</a>
+        case 'img':
+            return <img src={elem.src}/>
     }
+}
+
+class WelcomePageContent extends React.Component {
 
     render() {
+        const {elements} = content
         return (
             <div>
-
+                {elements.map((elem, idx) => <RenderElement key={idx} elem={elem}/>)}
             </div>
         );
     }
 }
 
-export default withRouter(ComponentsListView);
+export default withRouter(WelcomePage);
