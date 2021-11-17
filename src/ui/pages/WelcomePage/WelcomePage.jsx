34660eb08b0e77aea6bae010f2e18319cafd71b2
Trang Vu Thi <trang.vuthi@idealo.de>
2021-11-17 15:33:14 +0100

    "first attempt on WelcomePage.jsx"

--- src/ui/pages/WelcomePage/WelcomePage.jsx
+++ src/ui/pages/WelcomePage/WelcomePage.jsx
@@ -1,32 +1,56 @@
 import React from 'react'
 import content from './content.json'
+import {withRouter} from "react-router";
+import {fetchUserInfo} from "../BlogPage/data";
+import LoginMessage from "../../components/LoginMessage/LoginMessage";
 
-function RenderElement(props) {
-    const elem = props.elem
+export class WelcomePage extends React.Component {
 
-    switch (elem.type) {
-        case 'p':
-            return <p dangerouslySetInnerHTML={{__html: elem.content}}/>
-        case 'h1':
-            return <h1 dangerouslySetInnerHTML={{__html: elem.content}}/>
-        case 'h3':
-            return <h3 dangerouslySetInnerHTML={{__html: elem.content}}/>
-        case 'h4':
-            return <h4 dangerouslySetInnerHTML={{__html: elem.content}}/>
-        case 'h5':
-            return <h5 dangerouslySetInnerHTML={{__html: elem.content}}/>
-        case 'img':
-            return <img src={elem.src}/>
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: null,
+            error: null,
+        }
+    }
+
+    async componentDidMount() {
+        const user = await fetchUserInfo()
+
+        if(user.user){
+            this.setState({
+                user: user,
+            });
+        }else{
+            this.setState({
+                error: '401',
+            })
+        }
+    }
+
+    render() {
+        if(this.state.user){
+            return <WelcomePage {...this.props}/>
+        }else if(this.state.error){
+            return <LoginMessage children="Library"/>
+        }else{
+            return <h2>Loading...</h2>
+        }
     }
 }
 
-export default function WelcomePage() {
+class Welcome extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
-    const {elements} = content
+    render() {
+        return (
+            <div>
 
-    return (
-        <>
-            {elements.map((elem, idx) => <RenderElement key={idx} elem={elem}/>)}
-        </>
-    )
+            </div>
+        );
+    }
 }
+
+export default withRouter(ComponentsListView);
